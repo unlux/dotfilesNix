8fb511e75272603b811cd0d033d3402388c034c8
lakshay choudhary <lakshaychoudhary77712@gmail.com>
2024-11-23 13:40:03 +0530

    added keybinds for monitor brightness control

--- nixos-config/modules/gnome/keybindings.nix
+++ nixos-config/modules/gnome/keybindings.nix
@@ -1,14 +1,12 @@
-{ home-manager, ... }:
-let
+{home-manager, ...}: let
   username = "lux";
-in
-{
+in {
   # ---- Home Configuration ----
   home-manager.users.${username} = {
     dconf.settings = {
       # Keybindings
       "org/gnome/desktop/wm/keybindings" = {
-        close = [ "<Super>w" ];
+        close = ["<Super>w"];
         move-to-monitor-down = "disabled";
         move-to-monitor-left = "disabled";
         move-to-monitor-right = "disabled";
@@ -31,16 +29,28 @@ in
         name = "File Manager";
       };
 
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
-        binding = "<ctrl><alt>o";
-        command = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
-        name = "Volume UP";
+      # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      #   binding = "<ctrl><alt>o";
+      #   command = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
+      #   name = "Volume UP";
+      # };
+
+      # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+      #   binding = "<ctrl><alt>i";
+      #   command = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+      #   name = "Volume Down";
+      # };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
+        binding = "<Control><Alt><Super>equal";
+        command = "ddcutil setvcp 0x10 + 10";
+        name = "Monitor Brightness UP";
       };
 
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
-        binding = "<ctrl><alt>i";
-        command = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
-        name = "Volume Down";
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5" = {
+        binding = "<Control><Alt><Super>minus";
+        command = "ddcutil setvcp 0x10 - 10";
+        name = "Monitor Brightness Down";
       };
 
       # This is necessary for some reason, or the above custom-keybindings don't work.
@@ -50,6 +60,8 @@ in
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/"
         ];
       };
 
