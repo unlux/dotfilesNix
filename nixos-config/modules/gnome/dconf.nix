3da9f67283f58f125e7669b04dd7794fdef8f569
lakshay choudhary <lakshaychoudhary77712@gmail.com>
2025-06-26 13:42:46 +0530

    added kubectl aliases, declared almost all dconf settings

--- nixos-config/modules/gnome/dconf.nix
+++ nixos-config/modules/gnome/dconf.nix
@@ -1,9 +1,216 @@
 # Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
-{username, ...}: {
+{
+  username,
+  lib,
+  ...
+}: {
   home-manager.users.${username} = {...}: {
     dconf.settings = {
+      "org/gnome/Geary" = {
+        migrated-config = true;
+      };
+
+      "org/gnome/TextEditor" = {
+        style-scheme = "stylix";
+      };
+
+      "org/gnome/desktop/app-folders" = {
+        folder-children = ["Utilities" "YaST" "Pardus" "cfd19496-468f-4a88-b52b-e550adcd85a3"];
+      };
+
+      "org/gnome/desktop/app-folders/folders/Pardus" = {
+        categories = ["X-Pardus-Apps"];
+        name = "X-Pardus-Apps.directory";
+        translate = true;
+      };
+
+      "org/gnome/desktop/app-folders/folders/Utilities" = {
+        apps = ["org.gnome.Decibels.desktop" "org.gnome.Calculator.desktop" "org.gnome.Calendar.desktop" "org.gnome.clocks.desktop" "org.gnome.Console.desktop" "ca.desrt.dconf-editor.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Extensions.desktop" "org.gnome.FileRoller.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "gnome-abrt.desktop" "gnome-system-log.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.Dictionary.desktop" "org.gnome.Evince.desktop" "org.gnome.fonts.desktop" "org.gnome.Usage.desktop" "vinagre.desktop"];
+        categories = ["X-GNOME-Utilities"];
+        name = "X-GNOME-Utilities.directory";
+        translate = true;
+      };
+
+      "org/gnome/desktop/app-folders/folders/YaST" = {
+        categories = ["X-SuSE-YaST"];
+        name = "suse-yast.directory";
+        translate = true;
+      };
+
+      "org/gnome/desktop/app-folders/folders/cfd19496-468f-4a88-b52b-e550adcd85a3" = {
+        apps = ["startcenter.desktop" "base.desktop" "calc.desktop" "draw.desktop" "impress.desktop" "math.desktop" "writer.desktop"];
+        name = "Office";
+      };
+
+      "org/gnome/desktop/interface" = {
+        clock-show-date = true;
+        clock-show-weekday = true;
+        color-scheme = "prefer-dark";
+        document-font-name = "vegur  10";
+        enable-hot-corners = true;
+        font-antialiasing = "rgba";
+        font-name = "Inter 11";
+        gtk-theme = "adw-gtk3";
+        monospace-font-name = "CommitMono Nerd Font Regular 11";
+        show-battery-percentage = true;
+      };
+
+      "org/gnome/desktop/peripherals/keyboard".numlock-state = true;
+      "org/gnome/desktop/peripherals/mouse".accel-profile = "flat";
+      "org/gnome/desktop/peripherals/touchpad".tap-to-click = true;
+      "org/gnome/desktop/privacy".remember-recent-files = false;
+      "org/gnome/desktop/screensaver".lock-enabled = false;
+      "org/gnome/desktop/session".idle-delay = 0;
+      "org/gnome/desktop/sound".allow-volume-above-100-percent = true;
+
+      "org/gnome/desktop/wm/keybindings" = {
+        move-to-monitor-down = "<Super>j";
+        move-to-monitor-up = "<Super>k";
+        switch-applications = [];
+        switch-applications-backward = [];
+        switch-windows = ["<Alt>Tab"];
+        switch-windows-backward = ["<Shift><Alt>Tab"];
+      };
+
+      "org/gnome/desktop/wm/preferences" = {
+        resize-with-right-button = true;
+      };
+
+      "org/gnome/eog/view" = {
+        background-color = "#191724";
+      };
+
+      "org/gnome/mutter" = {
+        workspaces-only-on-primary = true;
+      };
+
+      "org/gnome/nautilus/list-view" = {
+        default-zoom-level = "small";
+        use-tree-view = true;
+      };
+
+      "org/gnome/nautilus/preferences" = {
+        default-folder-viewer = "list-view";
+        migrated-gtk-settings = true;
+        search-filter-time-type = "last_modified";
+      };
+
+      "org/gnome/nautilus/window-state" = {
+        initial-size = [900 506];
+        initial-size-file-chooser = [900 506];
+        maximized = false;
+      };
+
+      "org/gnome/nm-applet/eap/01c98a7c-efd9-48d8-b6e7-dc0c90a34753" = {
+        ignore-ca-cert = false;
+        ignore-phase2-ca-cert = false;
+      };
+
+      "org/gnome/settings-daemon/plugins/color" = {
+        night-light-enabled = false;
+        night-light-schedule-automatic = false;
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/"];
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        binding = "<Super>t";
+        command = "ghostty";
+        name = "open-terminal";
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+        binding = "<Super>e";
+        command = "nautilus";
+        name = "File Manager";
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
+        binding = "<Control><Alt><Super>equal";
+        command = "ddcutil setvcp 0x10 + 10";
+        name = "Monitor Brightness UP";
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5" = {
+        binding = "<Control><Alt><Super>minus";
+        command = "ddcutil setvcp 0x10 - 10";
+        name = "Monitor Brightness Down";
+      };
+
+      "org/gnome/settings-daemon/plugins/power" = {
+        power-button-action = "nothing";
+        sleep-inactive-ac-type = "nothing";
+      };
+
+      "org/gnome/shell" = {
+        command-history = ["reboot"];
+        favorite-apps = ["zen-beta.desktop" "code.desktop" "legcord.desktop"];
+        last-selected-power-profile = "performance";
+        welcome-dialog-last-shown-version = "46.2";
+      };
+
       "org/gnome/shell/extensions/alphabetical-app-grid" = {
         folder-order-position = "start";
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell" = {
+        settings-version = 2;
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/appfolder" = {
+        brightness = 0.6;
+        sigma = 30;
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/applications" = {
+        blur = true;
+        dynamic-opacity = false;
+        sigma = 100;
+        whitelist = ["org.gnome.Extensions" "com.mitchellh.ghostty" "org.gnome.Nautilus" "code"];
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/coverflow-alt-tab" = {
+        pipeline = "pipeline_default";
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
+        blur = true;
+        brightness = 0.6;
+        pipeline = "pipeline_default_rounded";
+        sigma = 30;
+        static-blur = true;
+        style-dash-to-dock = 0;
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/lockscreen" = {
+        pipeline = "pipeline_default";
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/overview" = {
+        pipeline = "pipeline_default";
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/panel" = {
+        brightness = 0.6;
+        pipeline = "pipeline_default";
+        sigma = 30;
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/screenshot" = {
+        pipeline = "pipeline_default";
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/window-list" = {
+        brightness = 0.6;
+        sigma = 30;
+      };
+
+      "org/gnome/shell/extensions/com/github/hermes83/compiz-alike-magic-lamp-effect" = {
+        duration = 223.0;
+        x-tiles = 50.0;
+        y-tiles = 50.0;
       };
 
       "org/gnome/shell/extensions/com/github/hermes83/compiz-windows-effect" = {
@@ -15,6 +222,7 @@
 
       "org/gnome/shell/extensions/dash-to-panel" = {
         animate-appicon-hover = true;
+        animate-appicon-hover-animation-extent = ''{"RIPPLE":4,"PLANK":4,"SIMPLE":1}'';
         appicon-margin = 0;
         appicon-padding = "4";
         appicon-style = "NORMAL";
@@ -49,6 +257,8 @@
         prefs-opened = false;
         primary-monitor = "ACR-4429030723W01";
         status-icon-padding = -1;
+        stockgs-keep-top-panel = false;
+        stockgs-panelbtn-click-only = false;
         trans-panel-opacity = 0.0;
         trans-use-custom-bg = false;
         trans-use-custom-opacity = true;
@@ -72,10 +282,15 @@
       };
 
       "org/gnome/shell/extensions/gsconnect" = {
+        devices = [];
         discoverable = false;
         enabled = true;
         id = "392dd798-c242-46e5-a856-b5f3fc31b295";
         name = "leptup";
+      };
+
+      "org/gnome/shell/extensions/gsconnect/preferences" = {
+        window-maximized = false;
       };
 
       "org/gnome/shell/extensions/lockscreen-extension" = {
@@ -90,12 +305,6 @@
         mouse-action-double = "PLAY_PAUSE";
         show-control-icons = false;
         show-player-icon = true;
-      };
-
-      "org/gnome/shell/extensions/com/github/hermes83/compiz-alike-magic-lamp-effect" = {
-        duration = 223.0;
-        x-tiles = 50.0;
-        y-tiles = 50.0;
       };
 
       "org/gnome/shell/extensions/notification-timeout" = {
@@ -133,6 +342,44 @@
         show-temperature = false;
         show-voltage = false;
         update-time = 2;
+      };
+
+      "org/gtk/gtk4/settings/file-chooser" = {
+        date-format = "regular";
+        default-zoom-level = "small";
+        location-mode = "path-bar";
+        show-hidden = true;
+        sidebar-width = 140;
+        sort-column = "name";
+        sort-directories-first = true;
+        sort-order = "ascending";
+        type-format = "category";
+        view-type = "list";
+        window-size = [900 506];
+      };
+
+      "org/gtk/settings/file-chooser" = {
+        date-format = "regular";
+        location-mode = "path-bar";
+        show-hidden = false;
+        show-size-column = true;
+        show-type-column = true;
+        sidebar-width = 175;
+        sort-column = "name";
+        sort-directories-first = false;
+        sort-order = "ascending";
+        type-format = "category";
+        window-position = [35 32];
+        window-size = [1231 692];
+      };
+
+      "org/virt-manager/virt-manager/confirm" = {
+        forcepoweroff = false;
+      };
+
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = ["qemu:///system"];
+        uris = ["qemu:///system"];
       };
     };
   };
